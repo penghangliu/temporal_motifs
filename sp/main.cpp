f739366ea13e73731d3f3cd3c5d8290b8bbb7d50
Penghang <penghangliu@gmail.com>
2021-08-17 12:50:48 -0400

    shortest path

---
+++ sp/main.cpp
@@ -0,0 +1,56 @@
+//
+//  main.cpp
+//  tmc
+//
+//  Created by Penghang Liu on 11/24/19.
+//  Copyright © 2019 Penghang Liu. All rights reserved.
+//
+
+#include <iostream>
+#include "main.hpp"
+
+int main(int argc, char * argv[]) {
+    const auto t1 = chrono::steady_clock::now();
+    
+    string graph (argv[1]);
+    string gname = graph.substr (graph.find_last_of("/") + 1, graph.find_last_of(".") - graph.find_last_of("/") -1);
+    string target (argv[2]);
+    string tname = target.substr (target.find_last_of("/") + 1, target.find_last_of(".") - target.find_last_of("/") -1);
+    string out_file = "out_" + gname + "_" + tname;
+    FILE* fp = fopen (out_file.c_str(), "w");
+    
+//Read file
+    Graph G(graph);
+        
+    const auto t2 = chrono::steady_clock::now();
+//    print_time (fp, "Read data time: ", t2 - t1);
+    
+//Count
+    ifstream in(target);
+    string line;
+    
+    int i = 0;
+    while (getline(in, line)) {
+        if (line[0] != '%' && line[0] != '#'){
+            stringstream ss (line);
+            vertex u, v;
+            ss >> u >> v;
+            if (G.check(u,v)) {
+                G.removeEdge(u,v);
+            }
+            int d = G.distance(u,v);
+            fprintf(fp, "%s,%s,%d \n", u.c_str(), v.c_str(), d);
+            G.addEdge(u,v);
+        }
+        i++;
+        if (i % 1000 == 0) {
+            cout << i << endl;
+        }
+    }
+    
+    const auto t3 = chrono::steady_clock::now();
+//    print_time (fp, "End-to-end Time: ", t3 - t1);
+    
+    fclose (fp);
+    return 0;
+}
---
+++ sp/main.hpp
@@ -0,0 +1,156 @@
+//
+//  tmc.hpp
+//  tmc
+//
+//  Created by Penghang Liu on 11/25/19.
+//  Copyright © 2019 Penghang Liu. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <initializer_list>
+
+#include <assert.h>
+#include <errno.h>
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <random>
+#include <chrono>
+#include <sys/stat.h>
+
+using namespace std;
+
+typedef chrono::duration<double> tms;
+typedef string vertex;
+
+template <class T>
+inline void hash_combine(std::size_t & seed, const T & v)
+{
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+namespace std
+{
+    template<typename S, typename T> struct hash<pair<S, T>>
+    {
+        inline size_t operator()(const pair<S, T> & v) const
+        {
+            size_t seed = 0;
+            ::hash_combine(seed, v.first);
+            ::hash_combine(seed, v.second);
+            return seed;
+        }
+    };
+    
+    template<typename S, typename T> struct hash<vector<pair<S, T>>>
+    {
+        inline size_t operator()(const vector<pair<S, T>> &k) const
+        {
+            size_t seed = 0;
+            for (auto it=k.begin(); it != k.end(); ++it) {
+                hash_combine(seed, it->first);
+                hash_combine(seed, it->second);
+            }
+            return seed;
+        }
+    };
+}
+
+inline void print_time (FILE* fp, const string& str, tms t) {
+    fprintf (fp, "%s %.6lf\n", str.c_str(), t.count());
+    fflush(fp);
+}
+
+class Graph
+{
+    unordered_map<vertex, vector<vertex>> adj;
+    
+public:
+    Graph(string filename);
+    void addEdge(vertex u, vertex v);
+    void removeEdge(vertex u, vertex v);
+    int distance(vertex u, vertex v);
+    bool check(vertex u, vertex v);
+};
+
+Graph::Graph(string filename)
+{
+    ifstream in(filename);
+    string line;
+    
+    while (getline(in, line)) {
+        if (line[0] != '%' && line[0] != '#'){
+            stringstream ss (line);
+            vertex u, v;
+            ss >> u >> v;
+            if (u != v) {
+                adj[u].push_back(v);
+                adj[v].push_back(u);
+            }
+        }
+    }
+    for (auto it=adj.begin(); it!=adj.end(); ++it) {
+        it->second.erase(unique(it->second.begin(), it->second.end()),it->second.end());
+    }
+}
+
+void Graph::addEdge(vertex u, vertex v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void Graph::removeEdge(vertex u, vertex v)
+{
+    adj[u].erase(find(adj[u].begin(), adj[u].end(), v));
+    adj[v].erase(find(adj[v].begin(), adj[v].end(), u));
+}
+
+int Graph::distance(vertex u, vertex v)
+{
+    queue<vertex> open;
+    int lvl = 1;
+    int current = 1;
+    
+    open.push(u);
+    while (!open.empty()) {
+        vertex k = open.front();
+        open.pop();
+        current--;
+        
+        for (int i=0; i<adj[k].size(); i++) {
+            vertex x = adj[k][i];
+            if(x==v) return lvl;
+            open.push(x);
+        }
+        
+        if (current == 0) {
+            lvl++;
+            current = open.size();
+        }
+    }
+    return -1;
+}
+
+bool Graph::check(vertex u, vertex v)
+{
+    if (find(adj[u].begin(), adj[u].end(), v) != adj[u].end()) {
+        return true;
+    }
+    else return false;
+}
